6de5cc8e6aab8c274f7a64202b7c91e59ac1843b
Bayaa-hub <baya00bellounis@gmail.com>
2024-12-02 22:04:26 +0100

    Update rental.c

--- rental.c
+++ rental.c
@@ -87,3 +87,88 @@ Rental createRental()
 
     return rental_info;
 }
+int searchRental(int rentalID, Rental *foundRental)
+{
+
+    FILE *file = fopen("rental_info.bin", "rb");
+    if (file == NULL)
+    {
+        printf("Error: Could not open file.\n");
+        return 0;
+    }
+
+    Entete entete;
+    fseek(file, 0, SEEK_SET);
+    fread(&entete, sizeof(Entete), 1, file);
+
+    for (int i = 0; i < entete.nbblock; i++)
+    {
+        Rental_Block block;
+        fseek(file, sizeof(Entete) + sizeof(Rental_Block) * i, SEEK_SET);
+        fread(&block, sizeof(Rental_Block), 1, file);
+        for (int j = 0; j < block.NB; j++)
+        {
+            if (block.tab[j].rentalID == rentalID)
+            {
+                *foundRental = block.tab[j];
+                fclose(file);
+                return 1;
+            }
+        }
+    }
+
+    fclose(file);
+    printf("Rental with ID %d not found.\n", rentalID);
+    return 0;
+}
+void displayRental(rentalRam r) {
+    printf("Rental ID: %d\n", r.rentalID);
+    printf("Customer: %s %s\n", r.customer.firstName, r.customer.lastName);
+    printf("Contact Info: %s\n", r.customer.contactInfo);
+    printf("Game: %s\n", r.game.title);
+    printf("Rental Date: %02d/%02d/%04d\n", r.rentaldate.day, r.rentaldate.month, r.rentaldate.year);
+    printf("Return Date: %02d/%02d/%04d\n", r.rentaldate.day, r.rentaldate.month, r.rentaldate.year);
+    printf("Rental Price: %.2f\n", r.game.rentalPrice);
+}
+
+
+void saveBlocksToFile(const char *filename) {
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    Entete entete ;
+    entete.nbblock = 1;
+    entete.nins = 0;  
+    Rental_Block block ;
+    block.NB = 0; 
+
+    fwrite(&entete, sizeof(Entete), 1, file);
+    fwrite(&block, sizeof(Rental_Block), 1, file);
+
+    fclose(file);
+}
+
+void loadBlocksFromFile(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("Error opening file for reading.\n");
+        return;
+    }
+
+    Entete entete;
+    fread(&entete, sizeof(Entete), 1, file);
+
+    for (int i = 0; i < entete.nbblock; i++) {
+        Rental_Block block;
+        fread(&block, sizeof(Rental_Block), 1, file);
+        printf("Block %d:\n", i + 1);
+        displayBlock(block);
+    }
+
+    fclose(file);
+}
+
+
